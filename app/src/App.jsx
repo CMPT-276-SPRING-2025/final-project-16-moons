a42bce164fc209466bc08756e31f2e1e3b3c9294
fangjess <jessf.fang@gmail.com>
2025-03-18 21:40:39 -0700

    started navbar component and installed react-router-dom

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./css/App.css";
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
+		<div className='App'>
+			<Navbar />
 			<h1>Final Project</h1>
-		</>
+		</div>
 	);
 }
 
