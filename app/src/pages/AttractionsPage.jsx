3ca09516aee64f0a50657cd66f4f19cd9aa6dcba
Nicole Stuart <nds11@sfu.ca>
2025-03-20 06:35:38 -0700

    Added a search bar to the Attractions page. Dependencies to run npm added to package files as well.

--- app/src/pages/AttractionsPage.jsx
+++ app/src/pages/AttractionsPage.jsx
@@ -2,12 +2,16 @@
 
   import BackgroundImage from '../assets/attractions-background.jpg';
   import '../styles/Attractions.css';
+  import { SearchBar } from "../components/SearchBar.jsx";
 
 function Attractions() {
   return (
     <div className="attractions" style={{ backgroundImage: `url(${BackgroundImage})`}}>
       <div className="headerContainer">
         <h1>Attractions</h1>
+      </div>
+      <div className ="search-bar-container">
+        <SearchBar />
       </div>
     </div>
   )
