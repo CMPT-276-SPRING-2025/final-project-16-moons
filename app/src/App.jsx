2c28807d00dbdf93b7448ffaca0f704b2df3522d
Nicole Stuart <nds11@sfu.ca>
2025-03-20 02:44:28 -0700

    Incorpoated friles from interface branch.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import "./css/App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Calendar from "./pages/CalendarPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<h1>Final Project</h1>
-		</>
+		<div className='App'>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/calendar" element={<Calendar />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
 	);
 }
 
